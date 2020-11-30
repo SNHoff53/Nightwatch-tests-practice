0fb5db053545859ebb7c06669df82707bf2902b5
hoffs <stephanie.nhoffman7@gmail.com>
2020-11-29 18:26:24 -0800

    updated browser details to confirm nightwatch and initial test are running correctly. Tests passed.

--- tests/initialTest.js
+++ tests/initialTest.js
@@ -1,8 +1,8 @@
 module.exports = {
     'My first test case'(browser) {
         browser
-        .url('https://www.nytimes.com/')
-        .waitForElementVisible('css-hnzl8o')
-        .assert.containsText("css-hnzl8o", "Today's Paper");
+        .url('https://news.ycombinator.com/')
+        .waitForElementVisible('.hnname')
+        .assert.containsText(".hnname", "Hacker News");
     }
-}
+};
